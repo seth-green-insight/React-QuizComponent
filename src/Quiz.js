ee1ed7601a336319b5989e8bc4a44ac7048ab65a
Seth Green <seth.green@insight.com>
2022-03-16 13:16:46 -0400

    check quiz end work

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import QuizEnd from './QuizEnd';
 import QuizQuestion from './QuizQuestion'
 
 let quizData = require('./quiz_data.json')
@@ -11,14 +12,27 @@ class Quiz extends Component {
         this.state = {quiz_position: 1};
     }
 
-
     render(){
+
+        const isQuizEnd = () => {
+            if (this.state.quiz_position - 1 === quizData.quiz_questions.length) {
+                return true;
+            } else {
+                return false;
+            }
+        };
 
         return (
             <div>
-                <QuizQuestion 
-                    quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
-                />
+                {isQuizEnd && (
+                    <QuizEnd />
+                )}
+
+                {!isQuizEnd && (
+                    <QuizQuestion 
+                        quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+                    />
+                )}
             </div>
         )
 
